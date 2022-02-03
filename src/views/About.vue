e9fb826adcf9feb529e33f0b52ae859eed56b8c0
panteLx <sebi.stahl1811@live.com>
2022-02-03 16:14:33 +0100

    added axios

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <ul v-for="post in posts" v-bind:key="post.id">
+      <li class="font-bold">{{ post.title }}</li>
+      <p>{{ post.body }}</p>
+    </ul>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  methods: {
+    async getData() {
+      try {
+        const response = await axios.get(process.env.VUE_APP_BACKEND_API);
+        // JSON responses are automatically parsed.
+        this.posts = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+
+  created() {
+    this.getData();
+  },
+};
+</script>
